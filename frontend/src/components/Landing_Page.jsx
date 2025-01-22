7be7fe5ea51a51ac10aeda11cc41a7d6a464cb08
Raidiniom <raidiniom22PH@gmail.com>
2025-01-22 13:16:00 +0800

    gihinayhinay og sugod

--- frontend/src/components/Landing_Page.jsx
+++ frontend/src/components/Landing_Page.jsx
@@ -1,18 +1,22 @@
 import './styles/landing.css'
+import { Link } from 'react-router-dom';
 
 export default function Landing_Page() {
     
     return (
         <>
           <div className='landing-header'>
-            <h1 className='landing-title'>Read_MangaKas</h1>
+            <Link to='/'><h1 className='landing-title'>Read_MangaKas</h1></Link>
             <div className="routes">
-                <button>Login</button>
-                <button>Register</button>
+                <Link to='/login'><button>Login</button></Link>
+                <Link to='/register'><button>Register</button></Link>
             </div>
           </div>
 
-          <div className='landing-body'></div>
+          <div className='landing-body'>
+            {/* displays top rated mangas being reed all time on the site */}
+            <div className='top-manga-display'></div>
+          </div>
         </>
     )
 }
