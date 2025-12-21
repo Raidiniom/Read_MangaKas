2718ce03a75e35690d78b6fab4a003cbf604c724
raidiniom <raidiniom22ph@gmail.com>
2025-12-21 19:05:16 +0800

    Yeah it works my guy

--- frontend/src/components/Chapter_bar.jsx
+++ frontend/src/components/Chapter_bar.jsx
@@ -1,10 +1,17 @@
-export default function Chapter_bar() {
+import { Link } from "react-router-dom";
+import "../styles/chapter_bar.css";
+
+
+export default function Chapter_bar({ manga = {}, chapeterNumber }) {
 
     return (
         <>
-            <div className="bar-body">
-                {/*  */}
-            </div>
+            <Link to={`/manga/${manga.slug}/chapter/${chapeterNumber}`} style={{ textDecoration: 'none' }}>
+                <div className="bar-body">
+                    <h2>{manga.title}</h2>
+                    <h3>Chapter {chapeterNumber}</h3>
+                </div>
+            </Link>
         </>
     )
 }
--- frontend/src/pages/Chapter_page.jsx
+++ frontend/src/pages/Chapter_page.jsx
@@ -3,7 +3,7 @@ export default function Chapter_page() {
     return (
         <>
             <div className="chapter-body">
-                {/*  */}
+                <h2>Chapter Page</h2>
             </div>
         </>
     )
